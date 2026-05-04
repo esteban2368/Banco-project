a2c6ede0924f9e7b413fb2482ab223f387d74ca3
Esteban Diaz <esteban.d2168@gmail.com>
2026-05-04 16:57:28 -0500

    feat: contexto usuario se agregó estado inicial al almacenado en el localstorage.

--- src/features/auth/store/AuthContenxt.tsx
+++ src/features/auth/store/AuthContenxt.tsx
@@ -1,5 +1,7 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import type { User } from '../models/User';
+
+import { getLocalStorage, setLocalStorage } from '../../../shared/utils/localStorageUtil'
 
 interface AuthContextValue {
     user: User | null;
@@ -11,3 +13,25 @@ interface AuthContextValue {
 const TOKEN_KEY = 'auth_token';
 const USER_KEY  = 'auth_user';
 
+const AuthContext = createContext<AuthContextValue | null>(null);
+
+const AuthProvider = ({ children }: React.ReactNode) => {
+    const [user, setUser] = useState<User | null>(()=>{
+        const userStored = getLocalStorage(USER_KEY, null);
+        return userStored ? JSON.parse(userStored) : null;
+    });
+
+    const saveSession = (userData: User , token: string) => {
+        setLocalStorage(USER_KEY, userData);
+        setUser(userData)
+    }
+
+    const logout = () => {
+        
+    }
+
+    const getToken = () => {
+
+    }
+
+}
