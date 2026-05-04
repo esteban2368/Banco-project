ba9b0c78a62778977ea29b306f792b53926cef0f
Esteban Diaz <esteban.d2168@gmail.com>
2026-05-03 21:33:02 -0500

    feat: Creación de componentes de rutas para dashboard y transfer

---
+++ src/pages/TransferPage.tsx
@@ -0,0 +1,9 @@
+const TransferPage = () => {
+    return (
+        <div>
+            <h1>Transferencia</h1>
+        </div>
+    );
+};
+
+export default TransferPage;
---
+++ src/shared/utils/localStorageUtil.ts
@@ -0,0 +1,14 @@
+const getLocalStorage = <T>(key: string, initialValue: T): T => {
+    try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+        return initialValue;
+    }
+};
+
+const setLocalStorage = <T>(key: string, value: T): void => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+};
+
+export { getLocalStorage, setLocalStorage };
