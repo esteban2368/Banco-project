b9c8dcd5955d6eb6329e79324ad4f4d0915bc0be
Esteban Diaz <esteban.d2168@gmail.com>
2026-05-04 19:23:55 -0500

    feat: Manejo de errores en showBalance.

--- src/features/transfer/components/ShowBalance.tsx
+++ src/features/transfer/components/ShowBalance.tsx
@@ -1,7 +1,15 @@
-export const ShowBalance = () => {
+
+import type { Balance } from "../models/Transfer";
+
+export const ShowBalance = ({ balancePromise }: { balancePromise: Promise<Balance> }) => {
+
+    if (!result.success) {
+        return <p className="error">{result.message}</p>;
+    }
     return (
         <div>
             <h1>Show Balance</h1>
+
         </div>
     );
 };
