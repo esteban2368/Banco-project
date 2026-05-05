270bef6f70d2ca2c0935a738a3b127a9346a97c3
Esteban Diaz <esteban.d2168@gmail.com>
2026-05-05 08:07:44 -0500

    feact: integración de iconos, ajuste de layout de formulario de ingreso.

--- src/features/auth/components/FormLogin.tsx
+++ src/features/auth/components/FormLogin.tsx
@@ -1,5 +1,6 @@
 import { Input } from "../../../shared/components/Input";
 import { Button } from "../../../shared/components/Button";
+import { Mail, Eye } from "lucide-react";
 
 import { useLogin } from "../hooks/useLogin";
 
@@ -8,9 +9,11 @@ export const FormLogin = () => {
 
     return (
         <div className="w-full max-w-md mx-auto rounded-2xl bg-white p-8 shadow-md border border-gray-100">
-            <form action={dispatch}>
-                <Input label="Email" name="email" />
-                <Input label="Password" name="password" type="password" />
+            <p className="text-2xl text-primary font-medium text-center">Bienvenido</p>
+            <p className="text-sm text-primary font-light text-center">Ingresa tus credenciales</p>
+            <form action={dispatch} className="flex flex-col gap-5">
+                <Input label="Email" name="email" leftIcon={<Mail size={18}/>} />
+                <Input label="Password" name="password" type="password" leftIcon={<Eye size={18}/>}/>
                 <Button type="submit" disabled={isPending} className="w-full">
                     {isPending ? "Ingresando" : "Ingresar"}
                 </Button>
