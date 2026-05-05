47e1aad99257b3f2774f486a76c491dbb469a1fd
Esteban Diaz <esteban.d2168@gmail.com>
2026-05-04 23:54:03 -0500

    feat: Creación de estado para filtros de listado y función para actualizar el estado.

--- src/features/transfer/components/TableListTransfer.tsx
+++ src/features/transfer/components/TableListTransfer.tsx
@@ -1,10 +1,24 @@
-import { use } from "react";
+import { use, useState } from "react";
 import { transferService } from '../services/transferService';
 
 type GetListTransferPromise = ReturnType<typeof transferService.getTransfers>;
 
 export const TableListTransfer = ({ promiseListTransfer }: { promiseListTransfer: GetListTransferPromise  }) => {
     const listTransfer = use(promiseListTransfer);
+    const [ filters, setFilters ] = useState({
+        value: '',
+        date: '',
+        payeerName: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFilters({
+            ...filters,
+            [name]: value
+        })
+    }
+
     return (
 
     )
