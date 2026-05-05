d4c335f36fa7ae6759785b36edd7770785f3ff0f
Esteban Diaz <esteban.d2168@gmail.com>
2026-05-05 12:00:23 -0500

    fix: ajuste de readme con instrucciones de ejecución.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,6 @@ import { FormLogin } from "../features/auth/components/FormLogin";
 const LoginPage = () => {
     return (
         <section className="flex flex-col align-items-center justify-center h-svh gap-5">
-            <h2>BancoXYZ</h2>
             <FormLogin />
         </section>
     );
